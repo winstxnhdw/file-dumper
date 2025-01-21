3bcf1e4f7532212036a9ed70c5821a0cd889bcbf
winstxnhdw <winston.ho@redbulltechnology.com>
2025-01-21 17:37:41 +0000

    ci/refactor: add biome and CI pipeline

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # file-dumper
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3dde66de-6b7f-4237-b2a7-0d683368da4d/deploy-status)](https://app.netlify.com/sites/file-dumper/deploys)
+[![main.yml](https://github.com/winstxnhdw/file-dumper/actions/workflows/main.yml/badge.svg)](https://github.com/winstxnhdw/file-dumper/actions/workflows/main.yml)
+[![formatter.yml](https://github.com/winstxnhdw/file-dumper/actions/workflows/formatter.yml/badge.svg)](https://github.com/winstxnhdw/file-dumper/actions/workflows/formatter.yml)
+
 `file-dumper` is a tiny vanilla TypeScript Vite application for dumping the contents of *any* file. This application is made for iOS users to read files with an unrecognisable extension.
 
 ## Development
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 function readFileAsync(file: File | Blob): Promise<string> {
-	return new Promise<string>((resolve, reject) => {
-		const reader = new FileReader();
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader()
 
-		reader.onload = () => resolve(reader.result as string);
-		reader.onerror = () => reject(reader.error);
-		reader.readAsText(file);
-	});
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = () => reject(reader.error)
+    reader.readAsText(file)
+  })
 }
 
 async function dumpFile() {
-	const fileInput = document.getElementById("file-input") as HTMLInputElement;
-	const file = fileInput.files?.[0];
+  const fileInput = document.getElementById('file-input') as HTMLInputElement
+  const file = fileInput.files?.[0]
 
-	if (!file) {
-		alert("You must pass a single file!");
-		return;
-	}
+  if (!file) {
+    alert('You must pass a single file!')
+    return
+  }
 
-	const fileContent = await readFileAsync(file);
-	const newFileBlob = new Blob([fileContent], { type: "text/plain" });
-	open(URL.createObjectURL(newFileBlob), file.name);
+  const fileContent = await readFileAsync(file)
+  const newFileBlob = new Blob([fileContent], { type: 'text/plain' })
+  open(URL.createObjectURL(newFileBlob), file.name)
 }
 
-window.dumpFile = dumpFile;
+window.dumpFile = dumpFile
