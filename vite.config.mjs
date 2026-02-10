63b0976ac1af87281dc1f507ceeba92e25f00aeb
winstxnhdw <winstxnhdw@gmail.com>
2026-02-11 02:03:23 +0800

    fix: set `autoUpdate`

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,34 @@ import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const manifest = {
+  name: 'File Dumper',
+  short_name: 'File Dumper',
+  description: 'A simple file dumper',
+  theme_color: '#ffffff',
+  icons: [
+    {
+      src: 'android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: 'android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+  ],
+}
+
 export default defineConfig({
   build: { minify: true },
-  plugins: [createHtmlPlugin({ minify: true }), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    createHtmlPlugin({ minify: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      manifest: manifest,
+      workbox: { navigateFallback: 'index.html' },
+    }),
+  ],
 })
